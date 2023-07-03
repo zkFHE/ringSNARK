a79e1388bcd27282af608a5cf44eccee07e9ccd9
Christian Knabenhans <christian@knabenhans.fr>
2023-07-03 16:46:06 +0200

    Add utility function to automatically generate ZKP-batching-friendly SEAL moduli, Make print_params() throw by default for invalid params

--- ringsnark/seal/seal_util.hpp
+++ ringsnark/seal/seal_util.hpp
@@ -7,8 +7,32 @@
 
 using namespace std;
 using namespace seal;
+/***
+ *
+ * @param poly_modulus_degree           degree of the outer FHE scheme
+ * @param inner_poly_modulus_degree     degree of the inner FHE schemes, used
+ * inside the ZKP
+ * @return a `vector<Modulus>' that has the same bit size as
+ * `CoeffModulus::BFVDefault(poly_modulus_degree)', but such that each for each
+ * sub-modulus q_i, q_i = 1 mod 2*inner_poly_modulus_degree. This guarantees
+ * that the batching optimization can be used for the ZKP scheme.
+ */
+vector<seal::Modulus> default_double_batching_modulus(
+    const size_t poly_modulus_degree, const size_t inner_poly_modulus_degree) {
+  assert(inner_poly_modulus_degree >= poly_modulus_degree);
+  auto default_modulus = CoeffModulus::BFVDefault(poly_modulus_degree);
+  vector<int> bit_sizes(default_modulus.size());
+  for (size_t i = 0; i < default_modulus.size(); i++) {
+    bit_sizes[i] = default_modulus[i].bit_count();
+  }
+  vector<Modulus> coeff_modulus = CoeffModulus::Create(
+      inner_poly_modulus_degree,
+      bit_sizes);  // Will be batching-friendly for inner scheme
+  return coeff_modulus;
+}
 
-void print_params(const EncryptionParameters parms) {
+void print_params(const EncryptionParameters parms,
+                  bool throw_on_invalid = true) {
   const SEALContext context(parms);
   cout << "[PARAM] Parameter validation (" << context.parameter_error_name()
        << "): " << context.parameter_error_message() << endl;
@@ -40,6 +64,13 @@ void print_params(const EncryptionParameters parms) {
     cout << q_i.value();
   }
   cout << endl;
+
+  if (throw_on_invalid &&
+      context.first_context_data()->qualifiers().parameter_error !=
+          seal::EncryptionParameterQualifiers::error_type::success) {
+    throw invalid_argument(string(context.parameter_error_name()) + ": " +
+                           string(context.parameter_error_message()));
+  }
 }
 
 #endif  // RINGSNARK_SEAL_UTIL_HPP
